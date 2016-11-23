c3a1039f433c87744aa00931041b36ff8f416f16
thekingcommander <c.yak32000@gmail.com>
2016-11-23 11:17:01 +0100

    sec

--- src/main/java/geco/src/main/java/geco/LoginGenerator.java
+++ src/main/java/geco/src/main/java/geco/LoginGenerator.java
@@ -4,7 +4,7 @@ import java.text.Normalizer;
 import java.util.regex.Pattern;
 
 /**
- * Classe representant un g enerateur de login
+ * Classe representant un generateur de login
  */
 public class LoginGenerator {
 
